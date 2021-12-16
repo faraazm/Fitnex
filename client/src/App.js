04a1bd2d9023a3e527e6d7a7297c781b9b46c525
faraazm <faraazmotiwala3@gmail.com>
2021-12-16 14:39:50 -0500

    RequireAuth infinite loop fix attempt AGAIN

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import axios from 'axios'
 
@@ -10,16 +16,17 @@ import SignIn from './landing/SignIn'
 import Onboarding from './components/Onboarding'
 import Dashboard from './components/Dashboard'
 
-import { AuthProvider } from "./hooks/useAuth"
+import { AuthProvider } from './hooks/useAuth'
 
 import './index.css'
 import './App.css'
 
 const App = () => {
-  let baseURL = 'http://localhost:8080'
-  if (Object.keys(process).length && process.env.NODE_ENV === 'production') {
-    baseURL = 'https://fitnex-workouts.herokuapp.com'
-  }
+  let baseURL =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8080'
+      : 'https://fitnex-workouts.herokuapp.com'
+
   axios.create({ baseURL, withCredentials: true }).interceptors.response.use(
     (response) => response,
     (error) => Promise.reject(error),
@@ -33,11 +40,13 @@ const App = () => {
   })
 
   return (
-    <MantineProvider theme={{
-      headings: {
-        fontFamily: 'Poppins, sans-serif'
-      }
-    }}>
+    <MantineProvider
+      theme={{
+        headings: {
+          fontFamily: 'Poppins, sans-serif',
+        },
+      }}
+    >
       <Router>
         <AuthProvider>
           <Routes>
@@ -46,16 +55,22 @@ const App = () => {
             <Route path="/contact" element={<Contact />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/onboarding" element={
-              <RequireAuth>
-                <Onboarding />
-              </RequireAuth>
-            } />
-            <Route path="/dashboard/*" element={
-              <RequireAuth>
-                <Dashboard />
-              </RequireAuth>
-            } />
+            <Route
+              path="/onboarding"
+              element={
+                <RequireAuth>
+                  <Onboarding />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/dashboard/*"
+              element={
+                <RequireAuth>
+                  <Dashboard />
+                </RequireAuth>
+              }
+            />
           </Routes>
         </AuthProvider>
       </Router>
@@ -65,20 +80,22 @@ const App = () => {
 
 function RequireAuth({ children }) {
   const token = localStorage.getItem('token')
-  const completedMeasurements = JSON.parse(localStorage.getItem('completedMeasurements'))
+  const completedMeasurements = JSON.parse(
+    localStorage.getItem('completedMeasurements'),
+  )
   const authenticated = token ? true : false
   const location = useLocation()
 
   // Checks if the user is authenticated and if they completed the onboarding flow
   if (authenticated && completedMeasurements) {
     return children
-  } else if (authenticated && !completedMeasurements && children.type.name === 'Onboarding') {
-    return children
   } else if (authenticated && !completedMeasurements) {
-    return <Navigate to="/onboarding" replace state={{ path: location.pathname }} />
-  } else {
-    return <Navigate to="/sign-in" replace state={{ path: location.pathname }} />
+    return (
+      <Navigate to="/onboarding" replace state={{ path: location.pathname }} />
+    )
   }
+
+  return <Navigate to="/sign-in" replace state={{ path: location.pathname }} />
 }
 
 export default App
